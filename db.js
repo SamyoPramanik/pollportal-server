96e32ba7f645da3943ac5e313af49a8c3549b4f6
SamyoPramanik <2105167@ugrad.cse.buet.ac.bd>
2025-04-06 23:59:14 +0600

    db.js fixed

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 import pkg from "pg";
 const { Pool } = pkg;
+import dotenv from "dotenv";
+dotenv.config();
 
 export const pool = new Pool({
-    user: "user",
-    host: "localhost",
-    database: "polldb",
-    password: "password",
-    port: 5432,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
 });
